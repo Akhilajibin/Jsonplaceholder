8b306eedf73fa60e21c2875351cf0414bc37ebb9
Akhilajibin <akhilacsivan94@gmail.com>
2019-03-26 22:10:38 +0530

    secondcommit

--- src/home.js
+++ src/home.js
@@ -1,36 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './home.css';
 
 class Home extends Component {
-constructor(props){
-	super(props);
-	this.state={
-	userId:'',
-	id:'',
-	title:'',
-	body:'',
-	}
-	}	
-ComponentDidMount()
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data:[],
+     
+    };
+  }
+
+
+clickMypost=()=>
 {
-	fetch("https://jsonplaceholder.typicode.com/posts")
-		.then(res=>res.json())
-		.then(response=>{
-		console.log(response);
-		alert(response.userId);
-		})
-		.catch(error=>console.log("error".error));
-		{/*alert(this.state.username);*/}
-	
+
+fetch('https://jsonplaceholder.typicode.com/posts?userId=5')
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data);
+        let info=data.map((item)=>{
+     
+          return (
+    		
+              <div key={item.id} className='maindiv'>
+            {/*  <p>Id : {item.id}</p>
+                <p>UserId : {item.userId}</p> */}
+                <p>Title : {item.title}</p>
+                <p>Body : {item.body}</p>
+
+              </div>
+
+            )
+        
+        
+       })
+        
+        this.setState({data:info});
+
+      })
+
+       .catch(error=>console.log("error",error));
 }
 
+
+
+
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data =>{
+        console.log(data);
+        let info=data.map((item)=>{
+       
+          return (
+    		
+              <div key={item.id} className='maindiv'>
+            {/*  <p>Id : {item.id}</p>
+                <p>UserId : {item.userId}</p>*/}
+                <p>Title : {item.title}</p>
+                <p>Body : {item.body}</p>
+
+              </div>
+
+            )
+        
+        
+       })
+        
+        this.setState({data:info});
+
+      })
+
+       .catch(error=>console.log("error",error));
+
+  }
+
   render() {
+    
     return (
       <div>
+      <h1>All posts</h1>
+      <button type="submit" onClick={this.clickMypost}>My Post</button>
+      {this.state.data}
+      
       </div>
     );
   }
 }
 
 export default Home;
+
